8e7b168b89c2ac6b5aa45400d11e8406d87e8ba0
Transfer cali.run <42573065+7pub@users.noreply.github.com>
2022-08-07 16:11:00 +0200

    The real crime would be not to finish what we started

--- _buk/preload.js
+++ _buk/preload.js
@@ -1,6 +1,5 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-
 var express = require('express');
 var app = express();
 
@@ -9,7 +8,9 @@ var port = 5003;
 
 app.use(express.static(path));
 app.get('*', function(req, res) {
-    res.sendFile(path + '/index.html');
+    req.path = __dirname + './main.js';
+    res.sendFile(path + './index.html');
+    //console.log(req);
 });
 app.listen(port);
-//require('child_process').exec('start http://localhost:5003/');
+require('child_process').exec('start http://localhost:5003/');
