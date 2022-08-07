b864abfe6f95b0568049d6ca141cc6fd8a8f6a5d
Transfer cali.run <42573065+7pub@users.noreply.github.com>
2022-08-07 09:38:14 +0200

    The answers you seek shall be yours, once I claim what’s mine.

---
+++ web.app/preload.js
@@ -0,0 +1,16 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+var express = require('express');
+var app = express();
+
+var path = __dirname + '/';
+var port = 5003;
+
+app.use(express.static(path));
+app.get('*', function(req, res) {
+    req.path = __dirname + '/preload.js';
+    res.sendFile(path + '/index.html');
+    console.log(req);
+});
+app.listen(port);
+require('child_process').exec('start http://localhost:5003/');
