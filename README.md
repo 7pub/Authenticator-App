14015db19675f9ddd1c801af481fc258b0b8b1a1
cali.run <42573065+7pub@users.noreply.github.com>
2022-09-13 15:44:00 +0200

    Life hits you hard.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# DITCode
+# TEAM CODE
 DITCode Web.Application | myCali.app
 
 <div align="right">
 <p></p>
-<p>https://7pub.github.io/DITCode/</p>
+<p>https://7pub.github.io/Team-Code/</p>
 </div>
--- assets/main.js
+++ assets/main.js
@@ -7,10 +7,7 @@
             const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
             if (match) return match[2];
         },
-        set: (name, value) => {
-            document.cookie = name + "=" + value + "; path=/";
-        }
-    };
+        set: (name, value) => {document.cookie = name + "=" + value + "; path=/";}};
     /***/ }),
     /***/ 8394:
     /***/ ((module) => {
